532252ac317547e20e31957718baa0c3b4afdd89
Grzegorz Kuśmirek <g.kusmirek95@gmail.com>
2019-06-09 23:14:21 +0200

    structure of sellerAgent added, doesnt work

--- src/main/kotlin/seller/SellerAgent.kt
+++ src/main/kotlin/seller/SellerAgent.kt
@@ -32,58 +32,89 @@ class SellerAgent : ModernAgent() {
 
     private lateinit var setup: SellerSetup
 
+    private var sellerState: Int = 0
+
+    private var request: ACLMessage = ACLMessage(ACLMessage.CFP)
+
     override fun onCreate(args: Array<String>) {
         setup = parseJsonFile(args[0])
 
         searchAirlineAgents().subscribeBy {
             val sequence = SequentialBehaviour().apply {
 
-                val request = ACLMessage(ACLMessage.CFP).apply {
-                    it.forEach { addReceiver(it.name) }
-                    content = toJSON(OfferRequest(from = setup.from, to = setup.to))
-                    replyWith = "OfferRequest-${System.currentTimeMillis()}"
-                }
-                // wyslanie requesta
-                addSubBehaviour(oneShot {
-                    send(request)
-                })
+                when(sellerState) {
 
-                // odebranie odpowiedzi do zapytania o oferte od linii lotniczych
-                addSubBehaviour(TimeoutReceiverBehaviour(
-                    template = MessageTemplate.MatchInReplyTo(request.replyWith),
-                    timeout = 5000,
-                    maxMessageCount = it.size,
-                    onFinished = { messages ->
-                        messages.forEach {
-                            proposeReplies.clear()
-                            proposeReplies.add(it)
+                    //Pobieramy nowe "zlecenie" na bilety i wysyłamy requesty do Agentów linii lotniczych
+                    0 -> {
+                        val offerRequest = getNextOfferRequest()
+                        request = ACLMessage(ACLMessage.CFP).apply {
+                            it.forEach { addReceiver(it.name) }
+                            content = toJSON(offerRequest)
+                            replyWith = "OfferRequest-${System.currentTimeMillis()}"
                         }
+                        // wyslanie requesta
+                        addSubBehaviour(oneShot {
+                            send(request)
+                        })
+                        sellerState = 1
                     }
-                ))
 
-                addSubBehaviour(oneShot {
-                    // parsowanie odpowiedzi do zapytania o ofertę
-                    val proposedOffers = proposeReplies
-                        .filter { it.performative == ACLMessage.PROPOSE }
-                        .groupBy { it.sender }
-                        .mapValues { fromJSON<Flight>(it.value.first().content) }
+                    // odebranie odpowiedzi do zapytania o oferte od linii lotniczych
+                    // jeśli nie otrzymano odpowiedzi - wróć do 0.
+                    1 -> {
+                        addSubBehaviour(TimeoutReceiverBehaviour(
+                            template = MessageTemplate.MatchInReplyTo(request.replyWith),
+                            timeout = 5000,
+                            maxMessageCount = it.size,
+                            onFinished = { messages ->
+                                messages.forEach {
+                                    proposeReplies.clear()
+                                    proposeReplies.add(it)
+                                }
+                            }
+                        ))
+                 //       sellerState = 2
+                 //   }
 
-                    // wybór najlepszej oferty (na podstawie ceny)
-                    val bestOffer = proposedOffers.minBy { it.value.price }?.toPair()
+                    //Wybieramy najlepszą ofertę i wysyłamy żądanie zakupu biletów
+                    //Jeśli znaleziono oferty z biletami - wybierz najlepszą i zarezerwuj bilety
+                    //Jeśli nie znaleziono oferty z zadanym biletem - trudno, wracamy do punktu 0.
+                  //  2 -> {
+                        addSubBehaviour(oneShot {
+                            // parsowanie odpowiedzi do zapytania o ofertę
+                            val proposedOffers = proposeReplies
+                                .filter { it.performative == ACLMessage.PROPOSE }
+                                .groupBy { it.sender }
+                                .mapValues { fromJSON<Flight>(it.value.first().content) }
 
-                    bestOffer?.let {
-                        log("Best offer from ${it.first.localName} = ${it.second}")
+                            // wybór najlepszej oferty (na podstawie ceny)
+                            val bestOffer = proposedOffers.minBy { it.value.price }?.toPair()
+
+                            bestOffer?.let {
+                                log("Best offer from ${it.first.localName} = ${it.second}")
+                            }
+
+                        })
                     }
-                })
 
+                    //Odbieramy info, czy udało się zakupić bilet
+                    //Jeśli nie, to wracamy do 2.
+                    //Jeśli tak, to do 0.
+                    3 -> {
+                        addSubBehaviour(oneShot {
 
-                addSubBehaviour(oneShot {
+                        })
+                    }
 
-                })
+                }
 
             }
             addBehaviour(sequence)
         }
+    }
+
+    private fun getNextOfferRequest(): OfferRequest {
+        return OfferRequest(from = setup.from, to = setup.to)
     }
 
     override fun onDestroy() = Unit
---
+++ src/main/kotlin/seller/SellerRequestsRepository.kt
@@ -0,0 +1,4 @@
+package pl.sag.seller
+
+class SellerRequestsRepository {
+}
