01c04bcff89757f8f142bf40e3e42e975eb50219
Jaroslaw Butajlo <jaroslaw.butajlo@allbright.io>
2019-06-16 13:17:04 +0200

    buy stats

--- src/main/kotlin/Stats.kt
+++ src/main/kotlin/Stats.kt
@@ -12,6 +12,8 @@ class Stats private constructor() {
         val INSTANCE by lazy { Stats() }
     }
 
+    val ticketsStats = mutableMapOf<Int, TicketsStats>()
+
     var statsState = StatsState(
         sellerAgentCount = 0,
         airlineAgentCount = 0
@@ -34,15 +36,33 @@ class Stats private constructor() {
     }
 
     fun onBuyRequest(seller: Agent, airline: Agent, buyRequest: BuyRequest) {
-
+        ticketsStats[buyRequest.flightId] = ticketsStats[buyRequest.flightId].let {
+            return@let if (it == null) {
+                TicketsStats(wantedToBuy = 1, actuallyBought = 0, buyRefused = 0)
+            } else {
+                it.copy(wantedToBuy = it.wantedToBuy + 1)
+            }
+        }
     }
 
     fun onBuyResponseSuccess(airline: Agent, seller: Agent, buySuccess: BuyResponseSuccess) {
-
+        ticketsStats[buySuccess.flightId] = ticketsStats[buySuccess.flightId].let {
+            return@let if (it == null) {
+                TicketsStats(wantedToBuy = 0, actuallyBought = 1, buyRefused = 0)
+            } else {
+                it.copy(actuallyBought = it.actuallyBought + 1)
+            }
+        }
     }
 
     fun onBuyResponseFailure(airline: Agent, seller: Agent, buyRefuse: BuyResponseRefuse) {
-
+        ticketsStats[buyRefuse.flightId] = ticketsStats[buyRefuse.flightId].let {
+            return@let if (it == null) {
+                TicketsStats(wantedToBuy = 0, actuallyBought = 0, buyRefused = 1)
+            } else {
+                it.copy(buyRefused = it.buyRefused + 1)
+            }
+        }
     }
 
 }
@@ -50,4 +70,10 @@ class Stats private constructor() {
 data class StatsState(
     var sellerAgentCount: Int,
     var airlineAgentCount: Int
+)
+
+data class TicketsStats(
+    val wantedToBuy: Int,
+    val actuallyBought: Int,
+    val buyRefused: Int
 )
